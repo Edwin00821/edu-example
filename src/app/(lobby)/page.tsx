bb40001a8d74d8f05b674a02325d2ac201b0563a
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2023-09-25 00:43:41 -0600

    adios mundo

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,6 +1,8 @@
 import { type FC } from 'react'
+import { redirect } from 'next/navigation'
 
 const Home: FC = () => {
+  redirect('/courses')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   )
